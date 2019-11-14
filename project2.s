772d57d12a26f45becb48e2226a3ea2e24ace856
sabingaire <46038463+sabingaire@users.noreply.github.com>
2019-11-14 13:14:27 -0500

    Created file project2.s with some .data entry

---
+++ project2.s
@@ -0,0 +1,6 @@
+.data
+#Updating the .data part where it includes the various output statements
+    Input_isLong: .asciiz "Input is too long."
+    emptyInput: .asciiz "Input is empty."
+    Input_isInvalid: .asciiz "Invalid input."
+    input_message: .asciiz "Choose a string: "
