a0665a0fcb589dfe278e54d373f388e0c36f1dea
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-11-14 18:34:15 -0500

    Treated empty strung as an invalid string

--- project2.s
+++ project2.s
@@ -7,7 +7,7 @@
 .data
 #Updating the .data part where it includes the various output statements
   Input_isLong: .asciiz "Invalid input"
-  emptyInput: .asciiz "Input is empty."
+  emptyInput: .asciiz "Invalid input"
   Input_isInvalid: .asciiz "Invalid input."
   input_message: .asciiz "Choose a string: "
 
