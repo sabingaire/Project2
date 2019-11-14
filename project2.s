3ace323e4737d052fbcef750f4b0c10b26c76e99
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-11-14 13:55:07 -0500

    MAde a space for user to inout string

--- project2.s
+++ project2.s
@@ -5,3 +5,9 @@
   emptyInput: .asciiz "Input is empty."
   Input_isInvalid: .asciiz "Invalid input."
   input_message: .asciiz "Choose a string: "
+
+#Creating the space for user to input 1000 characters
+  Maximum_store: .space 1000
+# Storing the first four non-space characters.
+#If the input has less than 4 characters, then the value would not be Invalid
+  Valid_store: .space 4
