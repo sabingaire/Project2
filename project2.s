94d11a6d9e97beb879af4b40b40d697ddcd2dbdd
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-11-14 14:44:55 -0500

    Checking null and empty

--- project2.s
+++ project2.s
@@ -36,3 +36,19 @@ four_characters:
     beq $a0, 0, loop1_exit_check     #As ascii value of Null is 0, so checking if the input is valid or not
 
     beq $a0, 10, loop1_exit_check      #checking if the input character is newline. the ascii value of the newline is 10
+
+    beq $a0, 32, four_characters
+#checking if the input character is a spacebar, i.e ' '. the ascii value of the space is 32
+
+    beq $t0, 1, Input_isLonglabel
+# if the input charcter is not space,newline or null, then it is either a valid charcter or invalid character. Regardless, this is the first character we analyse.
+#Then we take that and the next three characters after it in a row and also set the value of t0  to 1.
+# If we find another non-space character after that then the input is too long.
+
+#     li $t0, 1
+# # Setting the  value to 1 as before
+#
+# # Storing the characters as we had mentioned above
+#     la $t9, Valid_store
+#     lb $a0, -1($a1)
+#     sb $a0, 0($t9)
