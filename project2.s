84c7e9cfff9b0eec3a85671ccd21cbf3ef3badfe
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-11-14 13:51:46 -0500

    CHanged the message for printing if string is long

--- project2.s
+++ project2.s
@@ -1,7 +1,7 @@
 
 .data
 #Updating the .data part where it includes the various output statements
-  Input_isLong: .asciiz "Input is too long."
+  Input_isLong: .asciiz "Invalid input"
   emptyInput: .asciiz "Input is empty."
   Input_isInvalid: .asciiz "Invalid input."
   input_message: .asciiz "Choose a string: "
