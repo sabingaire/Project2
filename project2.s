4eb5fccf3674a236f22805217d1d6c5e033d369c
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-11-14 13:56:43 -0500

    Print the input string message

--- project2.s
+++ project2.s
@@ -11,3 +11,11 @@
 # Storing the first four non-space characters.
 #If the input has less than 4 characters, then the value would not be Invalid
   Valid_store: .space 4
+
+.text
+
+main:
+
+    li $v0, 4 						    #Make it ready to print the instruction to console
+    la $a0, input_message     #This will allow the "Choose a string method to print"
+    syscall
