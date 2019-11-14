5418e1866bd9f9392ca7f4d8c7eed5ced3305b14
Sabin Gaire <sabin.gaire@bison.howard.edu>
2019-11-14 13:40:12 -0500

    Added file project2.s

---
+++ project2.s
@@ -0,0 +1,7 @@
+
+.data
+#Updating the .data part where it includes the various output statements
+  Input_isLong: .asciiz "Input is too long."
+  emptyInput: .asciiz "Input is empty."
+  Input_isInvalid: .asciiz "Invalid input."
+  input_message: .asciiz "Choose a string: "
